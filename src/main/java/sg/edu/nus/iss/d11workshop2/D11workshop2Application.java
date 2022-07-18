5ca5b5e401382579405f8f06399e05f9667fe6a7
Tan_Ching_Le <tchinglewow@gmail.com>
2022-07-18 13:53:08 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/d11workshop2/D11workshop2Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.d11workshop2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D11workshop2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D11workshop2Application.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/d11workshop2/D11workshop2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.d11workshop2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D11workshop2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
